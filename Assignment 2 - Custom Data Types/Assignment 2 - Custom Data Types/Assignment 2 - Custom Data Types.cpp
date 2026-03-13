a8808b36718e65ca3972717b2db7d6b0d8165272
CallumWyn <165991642+CallumWyn@users.noreply.github.com>
2026-03-13 16:35:59 +1100

    Maths For Games Assignment 1
    Made a bunch of custom overloaded operators for a struct called vector 3

---
+++ Assignment 2 - Custom Data Types/Assignment 2 - Custom Data Types/Assignment 2 - Custom Data Types.cpp
@@ -0,0 +1,241 @@
+
+#include <iostream>
+
+struct Vector3 {
+    float x, y, z;
+
+    Vector3() { // Default Constructor
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    Vector3(const Vector3& i) { // Copy Constructor
+        x = i.x;
+        y = i.y;
+        z = i.z;
+    }
+
+    Vector3(float x1, float y1, float z1) { // Parameterised Constructor
+        x = x1;
+        y = y1;
+        z = z1;
+    }
+
+    Vector3 operator + (Vector3 v) {    // Addition
+        Vector3* v1 = new Vector3;
+        v1->x = x + v.x;
+        v1->y = y + v.y;
+        v1->z = z + v.z;
+        return *v1;
+    }
+
+    Vector3 operator - (Vector3 v) {    // Subtraction
+        Vector3* v1 = new Vector3;
+        v1->x = x - v.x;
+        v1->y = y - v.y;
+        v1->z = z - v.z;
+        return *v1;
+    }
+
+    Vector3 operator * (Vector3 v) {    // Multiplication
+        Vector3* v1 = new Vector3;
+        v1->x = x * v.x;
+        v1->y = y * v.y;
+        v1->z = z * v.z;
+        return *v1;
+    }
+
+    Vector3 operator / (Vector3 v) {    // Division
+        Vector3* v1 = new Vector3;
+        v1->x = x / v.x;
+        v1->y = y / v.y;
+        v1->z = z / v.z;
+        return *v1;
+    }
+
+    Vector3 operator * (float f) {      // Multiplication with Float
+        Vector3* v1 = new Vector3;
+        v1->x = x * f;
+        v1->y = y * f;
+        v1->z = z * f;
+        return *v1;
+    }
+
+    Vector3 operator / (float f) {      // Division with Float
+        Vector3* v1 = new Vector3;
+        v1->x = x / f;
+        v1->y = y / f;
+        v1->z = z / f;
+        return *v1;
+    }
+
+    Vector3 operator = (Vector3 v) {    // Assignment
+        x = v.x;
+        y = v.y;
+        z = v.z;
+        return *this;
+    }
+
+    Vector3 operator += (Vector3 v) {   // Addition Assignment
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return *this;
+    }
+
+    Vector3 operator -= (Vector3 v) {   // Subtraction Assignment
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
+    }
+
+    Vector3 operator *= (Vector3 v) {   // Multiplication Assignment
+        x *= v.x;
+        y *= v.y;
+        z *= v.z;
+        return *this;
+    }
+
+    Vector3 operator /= (Vector3 v) {   // Division Assignment
+        x /= v.x;
+        y /= v.y;
+        z /= v.z;
+        return *this;
+    }
+
+    Vector3 operator - () {             // Negative
+        x *= -1;
+        y *= -1;
+        z *= -1;
+        return *this;
+    }             
+
+    bool operator == (Vector3 v) {      // Equality
+        if ((x == v.x) && (y == v.y) && (z == v.z)) { return true; }
+        else { return false; }
+    }
+
+    bool operator != (Vector3 v) {      // Inequality
+        if ((x != v.x) || (y != v.y) || (z != v.z)) { return true; }
+        else { return false; }
+    }
+
+    float& operator [] (int i) {
+        if (i == 0) { return x; }
+        else if (i == 1) { return y; }
+        else if (i == 2) { return z; }
+    }
+
+    float Magnitude() {                 // Magnitude of the vector
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+
+    // This one has to go below the magnitude function as it uses the magnitude to figure out which one is smaller
+    bool operator < (Vector3 v) {      // Less than
+        if (Magnitude() < v.Magnitude()) { return true; }
+        else { return false; }
+    }
+
+    void Normalise() {
+        float magnitude = Magnitude();
+        x /= magnitude;
+        y /= magnitude;
+        z /= magnitude;
+    }
+
+    Vector3 Normalised() {
+        return Vector3(x / Magnitude(), y / Magnitude(), z / Magnitude());
+    }
+
+    Vector3 Dot() {}
+
+    Vector3 Cross() {}
+
+
+
+
+
+    
+
+};
+
+struct Vector4 {
+    float x, y, z, w;
+
+    Vector4() { // Default Constructor
+        x = 0;
+        y = 0;
+        z = 0;
+        w = 0;
+    }
+
+    Vector4(Vector4& i) { // Copy Constructor
+        x = i.x;
+        y = i.y;
+        z = i.z;
+        w = i.w;
+    }
+
+    Vector4(float x1, float y1, float z1, float w1) { // Parameterised Constructor
+        x = x1;
+        y = y1;
+        z = z1;
+        w = w1;
+    }
+
+};
+
+struct Matrix3 {
+
+};
+
+struct Matrix4 {
+
+};
+
+struct Color {
+    unsigned int R, G, B, A;
+
+    Color() { // Default Constructor
+        R = 0;
+        G = 0;
+        B = 0;
+        A = 255;
+    }
+
+    Color(Color& x) { // Copy Constructor
+        R = x.R;
+        G = x.G;
+        B = x.B;
+        A = x.A;
+    }
+
+    Color(unsigned int red, unsigned int green, unsigned int blue, unsigned int alpha) { // Parameterised Constructor
+        R = red;
+        G = green;
+        B = blue;
+        A = alpha;
+    }
+
+};
+
+
+
+int main()
+{
+    Vector3 a(13.5f, -48.23f, 862);
+    Vector3 b(1, 2, 3);
+
+    std::cout << a.Magnitude() << "\n";
+
+    a.Normalise();
+
+    std::cout << a.x << " - " << a.y << " - " << a.z << "\n";
+
+}
+
+
+
+
