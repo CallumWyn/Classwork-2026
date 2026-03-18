77b83f039b62632e0b62d0d7f3fe43f3c913febf
CallumWyn <165991642+CallumWyn@users.noreply.github.com>
2026-03-18 11:59:17 +1100

    Vector 4?
    Copied the code from vector 3 to vector 4, changed a little bit and all good? look back at this later

--- Assignment 2 - Custom Data Types/Assignment 2 - Custom Data Types/Assignment 2 - Custom Data Types.cpp
+++ Assignment 2 - Custom Data Types/Assignment 2 - Custom Data Types/Assignment 2 - Custom Data Types.cpp
@@ -113,7 +113,7 @@ struct Vector3 {
         y *= -1;
         z *= -1;
         return *this;
-    }             
+    }
 
     bool operator == (Vector3 v) {      // Equality
         if ((x == v.x) && (y == v.y) && (z == v.z)) { return true; }
@@ -157,7 +157,7 @@ struct Vector3 {
     }
 
     Vector3 Cross(Vector3 v1) {
-        Vector3 *v3 = new Vector3;
+        Vector3* v3 = new Vector3;
         v3->x = (y * v1.z) - (z * v1.y);
         v3->y = (z * v1.x) - (x * v1.z);
         v3->z = (x * v1.y) - (y * v1.x);
@@ -166,9 +166,9 @@ struct Vector3 {
 
     bool IsApproximatelyEqual(Vector3 v) {
 
-        
+
         if (//   Checks if v.x is between x +- 1e-4            Checks if v.y is between y +- 1e-4            Checks if v.z is between z +- 1e-4
-            ((x - 1e-4) <= v.x) && ((x + 1e-4) >= v.x)  && ((y - 1e-4) <= v.y) && ((y + 1e-4) >= v.y) && ((z - 1e-4) <= v.z) && ((z + 1e-4) >= v.z)) {
+            ((x - 1e-4) <= v.x) && ((x + 1e-4) >= v.x) && ((y - 1e-4) <= v.y) && ((y + 1e-4) >= v.y) && ((z - 1e-4) <= v.z) && ((z + 1e-4) >= v.z)) {
             return true;
         }
         return false;
@@ -182,7 +182,7 @@ struct Vector3 {
     }
 
     float Distance(Vector3 v) {
-        Vector3 *v2 = new Vector3;
+        Vector3* v2 = new Vector3;
         v2->x = -x + v.x;
         v2->y = -y + v.y;
         v2->z = -z + v.z;
@@ -191,7 +191,7 @@ struct Vector3 {
     }
 
     float Angle2D() {
-        return atan2(y, x) * (180/M_PI); // Multiplies arctan by 180/PI to convert it into degrees
+        return atan2(y, x) * (180 / M_PI); // Multiplies arctan by 180/PI to convert it into degrees
     }
 
 };
@@ -218,6 +218,179 @@ struct Vector4 {
         y = y1;
         z = z1;
         w = w1;
+    }
+
+    Vector4 operator + (Vector4 v) {    // Addition
+        Vector4* v1 = new Vector4;
+        v1->x = x + v.x;
+        v1->y = y + v.y;
+        v1->z = z + v.z;
+        v1->w = w;
+        return *v1;
+    }
+
+    Vector4 operator - (Vector4 v) {    // Subtraction
+        Vector4* v1 = new Vector4;
+        v1->x = x - v.x;
+        v1->y = y - v.y;
+        v1->z = z - v.z;
+        v1->w = w;
+        return *v1;
+    }
+
+    Vector4 operator * (Vector4 v) {    // Multiplication
+        Vector4* v1 = new Vector4;
+        v1->x = x * v.x;
+        v1->y = y * v.y;
+        v1->z = z * v.z;
+        v1->w = w;
+        return *v1;
+    }
+
+    Vector4 operator / (Vector4 v) {    // Division
+        Vector4* v1 = new Vector4;
+        v1->x = x / v.x;
+        v1->y = y / v.y;
+        v1->z = z / v.z;
+        v1->w = w;
+        return *v1;
+    }
+
+    Vector4 operator * (float f) {      // Multiplication with Float
+        Vector4* v1 = new Vector4;
+        v1->x = x * f;
+        v1->y = y * f;
+        v1->z = z * f;
+        v1->w = w;
+        return *v1;
+    }
+
+    Vector4 operator / (float f) {      // Division with Float
+        Vector4* v1 = new Vector4;
+        v1->x = x / f;
+        v1->y = y / f;
+        v1->z = z / f;
+        v1->w = w;
+        return *v1;
+    }
+
+    Vector4 operator = (Vector4 v) {    // Assignment
+        x = v.x;
+        y = v.y;
+        z = v.z;
+        w = v.w;
+        return *this;
+    }
+
+    Vector4 operator += (Vector4 v) {   // Addition Assignment
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return *this;
+    }
+
+    Vector4 operator -= (Vector4 v) {   // Subtraction Assignment
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return *this;
+    }
+
+    Vector4 operator *= (Vector4 v) {   // Multiplication Assignment
+        x *= v.x;
+        y *= v.y;
+        z *= v.z;
+        return *this;
+    }
+
+    Vector4 operator /= (Vector4 v) {   // Division Assignment
+        x /= v.x;
+        y /= v.y;
+        z /= v.z;
+        return *this;
+    }
+
+    Vector4 operator - () {             // Negative
+        x *= -1;
+        y *= -1;
+        z *= -1;
+        return *this;
+    }
+
+    bool operator == (Vector4 v) {      // Equality
+        if ((x == v.x) && (y == v.y) && (z == v.z) && (w == v.w)) { return true; }
+        else { return false; }
+    }
+
+    bool operator != (Vector4 v) {      // Inequality
+        if ((x != v.x) || (y != v.y) || (z != v.z) || (w != v.w)) { return true; }
+        else { return false; }
+    }
+
+    float& operator [] (int i) {
+        if (i == 0) { return x; }
+        else if (i == 1) { return y; }
+        else if (i == 2) { return z; }
+        else if (i == 3) { return w; }
+    }
+
+    float Magnitude() {                 // Magnitude of the vector
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+
+    // This one has to go below the magnitude function as it uses the magnitude to figure out which one is smaller
+    bool operator < (Vector4 v) {      // Less than
+        if (Magnitude() < v.Magnitude()) { return true; }
+        else { return false; }
+    }
+
+    void Normalise() {
+        float magnitude = Magnitude();
+        x /= magnitude;
+        y /= magnitude;
+        z /= magnitude;
+    }
+
+    Vector4 Normalised() {
+        return *(new Vector4(x / Magnitude(), y / Magnitude(), z / Magnitude(), w));
+    }
+
+    float Dot(Vector4 v) {
+        return x * v.x + y * v.y + z * v.z;
+    }
+
+    Vector4 Cross(Vector4 v1) {
+        Vector4* v3 = new Vector4;
+        v3->x = (y * v1.z) - (z * v1.y);
+        v3->y = (z * v1.x) - (x * v1.z);
+        v3->z = (x * v1.y) - (y * v1.x);
+        return *v3;
+    }
+
+    bool IsApproximatelyEqual(Vector4 v) {
+
+
+        if (//   Checks if v.x is between x +- 1e-4            Checks if v.y is between y +- 1e-4            Checks if v.z is between z +- 1e-4
+            ((x - 1e-4) <= v.x) && ((x + 1e-4) >= v.x) && ((y - 1e-4) <= v.y) && ((y + 1e-4) >= v.y) && ((z - 1e-4) <= v.z) && ((w + 1e-4) >= v.w) && ((w - 1e-4) <= v.w) && ((w + 1e-4) >= v.w)) {
+            return true;
+        }
+        return false;
+    }
+
+    float AngleBetween(Vector4 v) {
+        float cos = (Dot(v)) / (Magnitude() * v.Magnitude());
+        cos = acos(cos);
+
+        return cos;
+    }
+
+    float Distance(Vector4 v) {
+        Vector4* v2 = new Vector4;
+        v2->x = -x + v.x;
+        v2->y = -y + v.y;
+        v2->z = -z + v.z;
+        return v2->Magnitude();
+
     }
 
 };
@@ -257,15 +430,18 @@ struct Color {
 };
 
 void print(Vector3 v);
+void print(Vector4 v);
 
 
 int main()
 {
-    Vector3 a(1, 20, 3);
-    Vector3 b(-7, -4, -5);
+    Vector4 a(1, 20, 3, 1);
+    Vector4 b(-7, -4, -5, 51);
 
+    a += b;
 
-    std::cout << a.Angle2D();
+    print(a);
+
     //std::cout << a.x << " - " << a.y << " - " << a.z << "\n";
 
 }
@@ -275,5 +451,11 @@ void print(Vector3 v) {
     std::cout << "\n" << v.x << "\n";
     std::cout << v.y << "\n";
     std::cout << v.z << "\n";
+}
+void print(Vector4 v) {
+    std::cout << "\n" << v.x << "\n";
+    std::cout << v.y << "\n";
+    std::cout << v.z << "\n";
+    std::cout << v.w << "\n";
 }
 
